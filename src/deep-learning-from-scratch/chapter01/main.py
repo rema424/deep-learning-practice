cecb94ae9f58763d9d961e3689651c6c5c3b883b
rema424 <rema.gapp@gmail.com>
2019-01-20 14:08:10 +0900

    環境構築完了

--- src/deep-learning-from-scratch/chapter01/main.py
+++ src/deep-learning-from-scratch/chapter01/main.py
@@ -1,5 +1,24 @@
+# %%
 import numpy as np
-import matplotlib.puplot as puplot
+import matplotlib.pyplot as plt
+# from matplotlib.image import imread
 
 # データの生成
-np.
+x = np.arange(0, 6, 0.1)
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+# グラフの描画
+plt.plot(x, y1, label="sin")
+plt.plot(x, y2, label="cos", linestyle="--")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("sin & cos")
+plt.legend()
+
+# 画像の読み込み
+# img = imread('./../dataset/lena.png')
+# img = imread('./lena.png')
+# plt.imshow(img)
+
+plt.show()
